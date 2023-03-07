4d4d3bd0bbbbc41f558cddaa3abf8d547b226662
Tobi Tijani <tobi_tijani@yahoo.com>
2023-03-07 11:18:02 -0800

    Task 8 and 12 corrections

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,8 @@
 def uppercase(str):
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
-            print("{}".format(chr(ord(i) - 32)), end="")
+            c = chr(ord(i) - 32))
         else:
-            print("{}".format(i), end="")
-    print()
+            c = i
+        print("{}".format(c), end="")
+    print("")
