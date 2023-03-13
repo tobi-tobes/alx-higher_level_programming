58114b430cf4cd4364f60604939a24efc5210217
Tobi Tijani <tobi_tijani@yahoo.com>
2023-03-13 04:13:21 -0700

    Task 6: removed extra space before operator in line 5

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            if element  == row[len(row) - 1]:
+            if element == row[len(row) - 1]:
                 print("{:d}".format(element), end="")
             else:
                 print("{:d}".format(element), end=" ")
