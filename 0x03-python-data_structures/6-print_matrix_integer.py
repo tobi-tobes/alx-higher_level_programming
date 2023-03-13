740de58f75300f75666c3493d1880af21990c6fc
Tobi Tijani <tobi_tijani@yahoo.com>
2023-03-13 04:10:41 -0700

    Task 3 and 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            if element % 3 == 0:
+            if element  == row[len(row) - 1]:
                 print("{:d}".format(element), end="")
             else:
                 print("{:d}".format(element), end=" ")
