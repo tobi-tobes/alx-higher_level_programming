92a52a81b6ad1afa7df714a047dd23f9e6a1617a
Oluwatobi <tobi_tijani@yahoo.com>
2023-03-13 18:15:25 +0000

    Advanced task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints some basic info about Python lists.
+ * @p: Python object
+ * Return: Nothing
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, allocd, i;
+	PyObject *item;
+	const char *type;
+
+	size = PyList_size(p);
+	allocd = ((PyListObject *)(p))->allocated;
+
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", allocd);
+
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		type = item->ob_type->tp_name;
+		printf("Element %ld: %s\n", i, type);
+	}
+}
