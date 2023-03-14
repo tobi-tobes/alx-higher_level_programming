a37587092f337be45a8541bc34137bf44d852229
Tobi Tijani <tobi_tijani@yahoo.com>
2023-03-14 01:23:29 -0700

    Corrections

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    biggest = my_list[0]
     if len(my_list) == 0:
         return (None)
+    biggest = my_list[0]
     for i in range(1, len(my_list)):
         if my_list[i] > biggest:
             biggest = my_list[i]
