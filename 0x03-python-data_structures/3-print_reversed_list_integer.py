bc2c50104b1dad38d4d2f9544f73a9cf1cf0cb4f
Tobi Tijani <tobi_tijani@yahoo.com>
2023-03-14 01:38:27 -0700

    Task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
-    elif my_list[0] == None:
+    elif my_list is None:
         return (None)
     my_list.reverse()
     for i in my_list:
