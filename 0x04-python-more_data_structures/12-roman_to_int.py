161091a69d372e6a6aaf7b7bb1ca3ba2d1dc711c
Tobi Tijani <tobi_tijani@yahoo.com>
2023-03-15 15:31:00 -0700

    Tasks 12 and 14

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if roman_string is None:
         return (0)
-    elif isinstance(roman_string) is False:
+    elif isinstance(roman_string, str) is False:
         return (0)
     roman_dict = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     i, integer, less_than = 0, 0, 0
