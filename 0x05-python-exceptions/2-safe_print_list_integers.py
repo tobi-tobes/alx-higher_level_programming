7f626ac29aaf6c83b154629f0351a4e2a1676f8b
Tobi Tijani <tobi_tijani@yahoo.com>
2023-03-27 10:42:25 -0700

    Added TypeError Exception and put necessary new lines

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
             else:
                 continue
-    except IndexError:
+    except (IndexError, TypeError):
         pass
     print()
     return (count)
