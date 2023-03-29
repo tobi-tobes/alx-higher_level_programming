71ce314021362a0b27394843716962dde6724f02
Tobi Tijani <tobi_tijani@yahoo.com>
2023-03-28 19:41:17 -0700

    MagicClass

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
 import math
-"""Imports math module, which contains arithmetic functions and variables"""
-
-"""
-MagicClass Class
-This module defines a circle with the given circumference
-"""
 
 
 class MagicClass:
     """Represents a circle"""
 
     def __init__(self, radius=0):
-        """Creates an instance of MagicClass"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        """Calculates the area based on the given radius"""
-        return ((self.__radius ** 2) * math.pi)
+        return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-        """Calculates the circumference based on the given radius"""
-        return ((2 * math.pi) * self.__radius)
+        return (2 * math.pi * self.__radius)
