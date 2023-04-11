1713a7d0d25570cc2bbc4c664969bd3dbfd5db70
Tobi Tijani <tobi_tijani@yahoo.com>
2023-04-11 13:06:59 -0700

    Task 4: added check

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,6 @@ def inherits_from(obj, a_class):
     """returns True if the object is an instance of a class
 that inherited (directly or indirectly) the specified class;
  otherwise False."""
-    return issubclass(type(obj), a_class)
+    if isinstance(obj, a_class) and not type(obj) == a_class:
+        return True
+    return False
