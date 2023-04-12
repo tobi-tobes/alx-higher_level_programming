89f902eda178f20cf78041681ff6945f492f7cc2
Tobi Tijani <tobi_tijani@yahoo.com>
2023-04-12 11:30:19 -0700

    Task 13 correction: added check for if search_string was not found

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,6 +10,9 @@ each line containing a specific string
 def append_after(filename="", search_string="", new_string=""):
     """inserts a line of text to a file, after
  each line containing a specific string"""
+    if len(filename) == 0 or len(search_string) == 0:
+        return
+    found = False
     with open(filename, 'r', encoding="utf-8") as f:
         lines = f.readlines()
     for i in range(len(lines)):
@@ -19,7 +22,10 @@ def append_after(filename="", search_string="", new_string=""):
                              if len(new_string) > 0 else "\n")
             else:
                 lines.append(new_string if len(new_string) > 0 else "\n")
+            found = True
         else:
             continue
+    if found is False:
+        return
     with open(filename, 'w', encoding="utf-8") as f:
         f.writelines(lines)
