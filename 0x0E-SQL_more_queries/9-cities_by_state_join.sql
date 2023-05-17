d9c8320bf80b1b582220ec5223416e9447d5d85c
Tobi Tijani <tobi_tijani@yahoo.com>
2023-05-17 11:06:59 -0700

    Task 9 correction

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- A script that lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
-FROM hbtn_0d_usa.cities INNER JOIN hbtn_0d_usa.states
-ON hbtn_0d_usa.cities.state_id = hbtn_0d_usa.states.id
+FROM cities INNER JOIN states
+ON cities.state_id = states.id
 ORDER BY cities.id;
