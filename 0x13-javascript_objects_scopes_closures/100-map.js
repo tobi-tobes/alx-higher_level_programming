3a5d89223240b2272ae91bda837f4c828b8f1e07
Tobi Tijani <tobi_tijani@yahoo.com>
2023-06-14 13:57:34 -0700

    Task 11 fix

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
 const list = require('./100-data.js').list;
-const newList = list.map(function (num) {
-  return (num * list.indexOf(num));
-});
+const newList = list.map(num =>(num * list.indexOf(num));
 
 console.log(list);
 console.log(newList);
