f0732a3f0ce4cc1f9e1dc8f5a42289bcfda96762
Tobi Tijani <tobi_tijani@yahoo.com>
2023-06-14 14:31:26 -0700

    Removed indexOf and replacee with variable index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,14 @@
 #!/usr/bin/node
 
 const list = require('./100-data.js').list;
-const newList = list.map(num =>(num * list.indexOf(num)));
+let index = 0;
+const newList = list.map(function (num) {
+  const res = num * index;
+  if (index < list.length) {
+    index++;
+  }
+  return (res);
+});
 
 console.log(list);
 console.log(newList);
