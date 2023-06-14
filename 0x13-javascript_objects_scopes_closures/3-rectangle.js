4d80f0e0329afbd4a7408fa271b0b7a4aec35b5f
Tobi Tijani <tobi_tijani@yahoo.com>
2023-06-14 06:37:46 -0700

    Task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const h = this.height;
+    const w = this.width;
+    for (let i = 0; i < h; i++) {
+      let rect = '';
+      for (let j = 0; j < w; j++) {
+        rect = rect + 'X';
+      }
+      console.log(rect);
+    }
+  }
+};
