88af977e5204b23e2307e91a9310bd791ee950a2
Tobi Tijani <tobi_tijani@yahoo.com>
2023-06-14 14:00:06 -0700

    Fixed error with parentheses in Task 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const list = require('./100-data.js').list;
-const newList = list.map(num =>(num * list.indexOf(num));
+const newList = list.map(num =>(num * list.indexOf(num)));
 
 console.log(list);
 console.log(newList);
