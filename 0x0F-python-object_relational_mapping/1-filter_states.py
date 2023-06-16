e6cafded3c68787f119ea27bb457803b49e92440
Tobi Tijani <tobi_tijani@yahoo.com>
2023-06-16 06:37:33 -0700

    Task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+1-filter_states.py
+This file contains a script that lists all states with a
+name starting with N (upper N) from the database hbtn_0e_0_usa
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect("localhost", username, password, database)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+    db.close()
