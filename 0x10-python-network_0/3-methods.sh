39647010778dc4283bf0c0e76e8fc770583035c3
Tobi Tijani <tobi_tijani@yahoo.com>
2023-07-01 02:13:47 -0700

    Task 3: fixed grep statement to reflect the header parameter

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that takes in a URL and displays the methods the server will accept.
-curl -sIX OPTIONS "$1" | grep "^Allowed:\s" | cut -c8-
+curl -sIX OPTIONS "$1" | grep "^Allow:\s" | cut -c8-
