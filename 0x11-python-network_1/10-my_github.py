baa4db9bde6f6d96cf62c8ea73bae0b0ec848420
Tobi Tijani <tobi_tijani@yahoo.com>
2023-07-01 12:54:38 -0700

    Task 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+10-my_github.py
+This module contains a script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    url = f"https://api.github.com/users/{user}"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": pwd,
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    response = requests.get(url, headers=headers)
+    resp_id = response.json().get("id")
+    print(resp_id)
