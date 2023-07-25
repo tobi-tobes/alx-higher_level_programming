cf655f9244fe6751e271ceb986a3d0b6955ef888
Tobi <tobi_tijani@yahoo.com>
2023-07-25 09:48:32 -0700

    Task: added formatting to output

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -23,5 +23,14 @@ request(url, (error, response, body) => {
       }
     }
   }
-  console.log(output);
+  let formattedOutput = "{ ";
+  Object.keys(output).sort((a, b) => a - b).forEach(key => {
+      formattedOutput += `  '${key}': ${output[key]},\n`;
+  });
+  formattedOutput = formattedOutput.slice(0, -2);
+  formattedOutput += " }";
+  
+  formattedOutput = formattedOutput.replace(/\{(\s{2})/, "{");
+
+  console.log(formattedOutput);
 });
