0e7ac31af09c7809152c74e93196c4d242e75e76
Tobi Tijani <tobi_tijani@yahoo.com>
2023-08-02 13:00:19 -0700

    Task 4 fix

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,6 +1,7 @@
 $('DIV#toggle_header').on('click', function () {
   if ($('DIV#toggle_header').hasClass('green')) {
     $('DIV#toggle_header').removeClass('green');
+    $("DIV#toggle_header").addClass("red");
   } else {
     $('DIV#toggle_header').removeClass('red');
     $('DIV#toggle_header').addClass('green');
